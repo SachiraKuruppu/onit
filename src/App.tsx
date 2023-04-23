b462279c5d8d1c2a27d47674c2ba6436d6deab82
rsachira-abi <rsachira@live.com>
2023-04-23 12:14:40 +1200

    Add EsLint, make syntax consistent

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import 'reflect-metadata';
+import React, { type ReactElement } from "react";
+import "./App.css";
+import "reflect-metadata";
 
-function App() {
+function App(): ReactElement {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className='App-link'
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
